9e7b433d44d23b6392e8ec1ae4c6107b78e84f38
Aslıhan Özfidan <aslihanozfidan@gmail.com>
2017-06-17 03:38:01 +0300

    home page was completed

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+     /**
+     * @Route("/menu", name="menu")
+     */
+    public function menuAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('pages/menu/menu.html.twig', []);
+    }
 }
